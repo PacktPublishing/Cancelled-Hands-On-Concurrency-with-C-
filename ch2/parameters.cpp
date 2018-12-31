d502f54a6f7d6304e459543113a34aa25072bc88
Kevin B. Carpenter <kevin.carpenter@epx.com>
2018-12-30 19:04:45 -0700

    Initial

---
+++ ch2/parameters.cpp
@@ -0,0 +1,18 @@
+#include <thread>
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::vector<std::string> greetings {"Hola", "Gutentag", "Bonure", "Konnichiwa"};
+
+void hello( std::vector<std::string> greet, std::string var) {
+    for (auto g : greet) {
+	    std::cout << g << " " << var << std::endl;
+    }
+}
+
+int main( ) {
+	std::thread t1( hello, greetings, "World with Parameters" );
+	t1.join( );
+	return 0;
+}
